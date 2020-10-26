86dc9d98643bf2b7cbd72339d8352384b45be3fc
YundoChoi <48561168+zygn@users.noreply.github.com>
2020-10-26 22:09:20 +0900

    Update ICE_fgm_ChangSu_10_26.py

--- ICE_fgm_ChangSu_10_26.py
+++ ICE_fgm_ChangSu_10_26.py
@@ -69,8 +69,8 @@ class Pure_Pursuit:
         self.drive_pub = rospy.Publisher("/ICE/drive", AckermannDriveStamped, queue_size = 10 )
 
     def get_waypoint(self):
-        file_wps = np.genfromtxt('../f1tenth_ws/src/car_duri/wp_vegas.csv',delimiter=',',dtype='float')
-        # file_wps = np.genfromtxt('/catkin_ws/src/car_duri/wp_vegas.csv',delimiter=',',dtype='float')
+#         file_wps = np.genfromtxt('../f1tenth_ws/src/car_duri/wp_vegas.csv',delimiter=',',dtype='float')
+        file_wps = np.genfromtxt('/catkin_ws/src/car_duri/wp_vegas.csv',delimiter=',',dtype='float')
         temp_waypoint = []
         for i in file_wps:
             wps_point = [i[0],i[1],0]
